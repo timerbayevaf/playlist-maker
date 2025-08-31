f15f8652115d5381a1d4b0a9ef650084cd5b96bc
Timerbayev Aynur <timerbayev@gnivc.ru>
2025-09-01 02:48:07 +0300

    feat: добавление fragments

--- app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.playlistmaker.main.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
-import com.example.playlistmaker.medialibraries.ui.MediaLibrariesActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMainBinding
-import com.example.playlistmaker.search.ui.SearchActivity
-import com.example.playlistmaker.settings.ui.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
@@ -16,20 +15,9 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    binding.search.setOnClickListener {
-      val displayIntent = Intent(this, SearchActivity::class.java)
-      startActivity(displayIntent)
-    }
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+    val navController = navHostFragment.navController
 
-    binding.mediaLibrary.setOnClickListener {
-      val displayIntent = Intent(this, MediaLibrariesActivity::class.java)
-      startActivity(displayIntent)
-    }
-
-    binding.settings.setOnClickListener {
-      val displayIntent = Intent(this, SettingsActivity::class.java)
-      startActivity(displayIntent)
-    }
-
+    binding.bottomNavigationView.setupWithNavController(navController)
   }
 }
